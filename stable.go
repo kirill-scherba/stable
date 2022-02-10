16d1043130e3326602b66a2e9adf5d0927806d06
Kirill Scherba <kirill@scherba.ru>
2022-02-10 01:04:52 +0000

    Add columns format, totals and clean line

--- stable.go
+++ stable.go
@@ -16,15 +16,18 @@ type Stable struct {
 	aligns    []int
 	totals    []int
 	totalSPtr interface{}
+	cleanLine string
+	formats   []string
 }
 
 // title return list title string
-func (t Stable) title(in interface{}, lens []int, aligns []int) (str string) {
+func (t Stable) title(in interface{}, lens []int) (str string) {
 	val := reflect.Indirect(reflect.ValueOf(in))
 	l := val.NumField()
+	str = t.cleanLine
 	for i := 0; i < l; i++ {
-		s := fmt.Sprint(strings.ToUpper(val.Type().Field(i).Name)) // + " "
-		if len(aligns) > i && aligns[i] > 0 {
+		s := fmt.Sprint(strings.ToUpper(val.Type().Field(i).Name))
+		if len(t.aligns) > i && t.aligns[i] > 0 {
 			str += fmt.Sprintf("%*s", lens[i], s)
 		} else {
 			str += fmt.Sprintf("%-*s", lens[i], s)
@@ -37,15 +40,56 @@ func (t Stable) title(in interface{}, lens []int, aligns []int) (str string) {
 }
 
 // line return list body string
-func (t Stable) line(in interface{}, lens []int, aligns []int) (str string) {
+func (t Stable) line(in interface{}, lens []int) (str string) {
 	val := reflect.Indirect(reflect.ValueOf(in))
 	l := val.NumField()
+	str = t.cleanLine
 	for i := 0; i < l; i++ {
-		s := fmt.Sprint(val.Field(i))
-		if len(aligns) > i && aligns[i] > 0 {
+		s := fmt.Sprintf(t.getFormat(i), val.Field(i))
+		if len(t.aligns) > i && t.aligns[i] > 0 {
 			str += fmt.Sprintf("%*s", lens[i], s)
 		} else {
 			str += fmt.Sprintf("%-*s", lens[i], s)
+		}
+		if i < l-1 {
+			str += "   "
+		}
+		t.totalAdd(i, val.Field(i))
+	}
+	return
+}
+
+// totalAdd add table line value to totals
+func (t Stable) totalAdd(i int, v reflect.Value) {
+	if i >= len(t.totals) || t.totals[i] == 0 {
+		return
+	}
+
+	val := reflect.Indirect(reflect.ValueOf(t.totalSPtr))
+
+	switch val.Field(i).Kind() {
+	case reflect.Int, reflect.Int64:
+		tot := val.Field(i).Int() + v.Int()
+		val.Field(i).SetInt(tot)
+	case reflect.Float64, reflect.Float32:
+		tot := val.Field(i).Float() + v.Float()
+		val.Field(i).SetFloat(tot)
+	}
+}
+
+// total return total string
+func (t Stable) total(lens []int) (str string) {
+	val := reflect.Indirect(reflect.ValueOf(t.totalSPtr))
+	l := len(t.totals)
+	for i := range t.totals {
+		if i == 0 {
+			str = "\n" + t.cleanLine
+		}
+		if t.totals[i] == 0 {
+			str += fmt.Sprintf("%*s", lens[i], "")
+		} else {
+			s := fmt.Sprintf(t.getFormat(i), val.Field(i))
+			str += fmt.Sprintf("%*s", lens[i], s)
 		}
 		if i < l-1 {
 			str += "   "
@@ -72,7 +116,7 @@ func (t Stable) lineLens(in interface{}, lens []int) []int {
 	val := reflect.Indirect(reflect.ValueOf(in))
 	l := val.NumField()
 	for i := 0; i < l; i++ {
-		ln := len(fmt.Sprint(val.Field(i)))
+		ln := len(fmt.Sprintf(t.getFormat(i), val.Field(i)))
 		if ln > lens[i] {
 			lens[i] = ln
 		}
@@ -118,6 +162,7 @@ func (t Stable) StructToTable(in interface{}) (str string) {
 				if lineFeedLeft {
 					str = "\n"
 				}
+				str += t.cleanLine
 				str += strings.Repeat("┈", l)
 				if !lineFeedLeft {
 					str += "\n"
@@ -128,12 +173,13 @@ func (t Stable) StructToTable(in interface{}) (str string) {
 		for i := 0; i < s.Len(); i++ {
 			if i == 0 {
 				str += line(sumLen, false)
-				str += t.title(s.Index(i).Interface(), lens, t.aligns)
+				str += t.title(s.Index(i).Interface(), lens)
 				str += line(sumLen, true)
 			}
-			str += "\n" + t.line(s.Index(i).Interface(), lens, t.aligns)
+			str += "\n" + t.line(s.Index(i).Interface(), lens)
 		}
 		str += line(sumLen, true)
+		str += t.total(lens)
 	}
 	return
 }
@@ -156,4 +202,28 @@ func (t *Stable) Totals(sptr interface{}, totals ...int) *Stable {
 	t.totals = totals
 	t.totalSPtr = sptr
 	return t
+}
+
+// CleanLine set clean line terminal command before all table rows
+func (t *Stable) CleanLine(cl bool) *Stable {
+	if cl {
+		t.cleanLine = "\033[K"
+	}
+	return t
+}
+
+// Format sets columns format
+func (t *Stable) Formats(formats ...string) *Stable {
+	t.formats = formats
+	return t
+}
+
+// getFormat retun column format
+func (t Stable) getFormat(col int) (fmt string) {
+	if col < len(t.formats) && t.formats[col] != "" {
+		fmt = t.formats[col]
+	} else {
+		fmt = "%v"
+	}
+	return
 }
--- stable_test.go
+++ stable_test.go
@@ -18,7 +18,7 @@ func TestStructToTable(t *testing.T) {
 	}
 	var a = []table{
 		{"ququ-1", "ququ-1_name", 1, 3.14, "ready", "1.1.1.1"},
-		{"ququ-2", "ququ-2_name", 2, 512.75, "not-ready", "22.2.2.2"},
+		{"ququ-2", "ququ-2_name", 2, 512.7, "not-ready", "22.2.2.2"},
 	}
 
 	st := new(Stable)
@@ -26,8 +26,10 @@ func TestStructToTable(t *testing.T) {
 	// print default table
 	fmt.Print(st.StructToTable(a) + "\n\n")
 
-	// print table with columns align and lines
-	var totals table
-	st.Lines().Aligns(0, 0, 1, 1, 1, 1).Totals(&totals, 0, 0, 1)
+	// print table with lines, columns align, column format and totals
+	st.Lines().
+		Aligns(0, 0, 1, 1, 1, 1).
+		Formats("", "", "", "%.2f").
+		Totals(&table{}, 0, 0, 1, 1)
 	fmt.Print(st.StructToTable(a) + "\n\n")
 }
